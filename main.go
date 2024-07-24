6d5858974a662cc8916d02fd37d724bf94656702
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-07-24 17:28:29 -0400

    Refactor main loop

--- main.go
+++ main.go
@@ -29,47 +29,16 @@ Infinite:
 		input = strings.TrimRight(input, " \n")
 		switch {
 		case strings.Contains(input, "new"):
-			words := strings.Split(input, " ")
-			name := ""
-			if len(words) > 1 {
-				name = words[1]
-			} else {
-				name = "Untitled"
-			}
-			text := note.GetText()
-			newNode := node.NewNode(name, text, nil)
+			newNode := createNewNode(input)
 			node.UpdateNodes(newNode, &nodes)
 		case strings.Contains(input, "remove"):
-			args := strings.Split(input, " ")
-			if len(args) > 1 {
-				args = args[1:]
-				for _, arg := range args {
-					err = node.DeleteNode(arg, &nodes)
-					if err != nil {
-						fmt.Println(err)
-					}
-				}
-			} else {
-				fmt.Println("Unable to delete node. Invalid arguments.")
-			}
+			removeNode(input, err, &nodes)
 		case input == "help":
 			note.HelpMenu()
 		case input == "show":
 			node.DisplayNodes(nodes)
 		case strings.Contains(input, "link"):
-			inputNodes := strings.Split(input, " ")
-			if len(inputNodes) > 2 {
-				node1, err1 := node.GetNode(inputNodes[1], nodes)
-				node2, err2 := node.GetNode(inputNodes[2], nodes)
-				if err1 != nil || err2 != nil {
-					fmt.Println("Error getting first two nodes:", err1, err2, "| Aborting link.")
-					continue
-				}
-				node.LinkNodes(node1, node2)
-				fmt.Println("Nodes linked successfully!")
-			} else {
-				fmt.Println("Unable to link nodes. Invalid arguments.")
-			}
+			linkNodes(input, nodes)
 		case input == "read":
 			nodes = cmd.ReadNodesFromFile()
 		case strings.Contains(input, "check"): 
@@ -87,5 +56,48 @@ Infinite:
 		default:
 			fmt.Println("Unknown command.")
 		}
+	}
+}
+
+func createNewNode(input string) node.Node {
+	words := strings.Split(input, " ")
+	name := ""
+	if len(words) > 1 {
+		name = words[1]
+	} else {
+		name = "Untitled"
+	}
+	text := note.GetText()
+	return node.NewNode(name, text, nil)
+}
+
+func removeNode(input string, err error, nodes *node.Nodes) {
+	args := strings.Split(input, " ")
+	if len(args) > 1 {
+		args = args[1:]
+		for _, arg := range args {
+			err = node.DeleteNode(arg, nodes)
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+	} else {
+		fmt.Println("Unable to delete node. Invalid arguments.")
+	}
+}
+
+func linkNodes(input string, nodes node.Nodes) {
+	inputNodes := strings.Split(input, " ")
+	if len(inputNodes) > 2 {
+		node1, err1 := node.GetNode(inputNodes[1], nodes)
+		node2, err2 := node.GetNode(inputNodes[2], nodes)
+		if err1 != nil || err2 != nil {
+			fmt.Println("Error getting first two nodes:", err1, err2, "| Aborting link.")
+			return
+		}
+		node.LinkNodes(node1, node2)
+		fmt.Println("Nodes linked successfully!")
+	} else {
+		fmt.Println("Unable to link nodes. Invalid arguments.")
 	}
 }
